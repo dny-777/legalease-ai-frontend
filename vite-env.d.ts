70dac717017e80d5f7b569b95e59989d801bd4d9
[dny-777] <[dnyanadathorat77@gmail.com]>
2025-10-29 16:15:16 +0530

    Fix TypeScript config and vite env types

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  // add any other env vars you might use like:
+  // readonly VITE_SOME_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module '*.module.css';
